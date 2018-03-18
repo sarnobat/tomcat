60ca7f0850e4c2c7682ea034a17e80f74b52a8be
ssarnobat <ss401533@gmail.com>
2018-03-17 21:31:31 -0700

    Minor changes (untested)

--- src/main/java/a/Main.java
+++ src/main/java/a/Main.java
@@ -27,7 +27,7 @@ public class Main {
         "SRIDHAR Main.main() - classpath = " + System.getProperty("java.class.path"));
 
     Tomcat server = new Tomcat();
-    int port = 8080;
+    int port = 8081;
     server.setPort(port);
 
     // Document root
